75388d144ed0eea0f13d3bf30a23c421e9fb2d9f
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2021-01-06 18:17:56 -0800

    small fix

--- pyompa/__init__.py
+++ pyompa/__init__.py
@@ -5,4 +5,4 @@ from .watermasspenaltyfunc import get_wm_penalty_func
 from .plotting import (plot_water_mass_fractions, plot_residuals,
                        plot_thermocline_water_mass_fractions,
                        plot_thermocline_residuals,
-                       build_altair_viz)
+                       build_altair_viz, build_thermocline_altair_viz)
