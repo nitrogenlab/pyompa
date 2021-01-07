dff40d5a88f1e0095edb9ed6c2b3b839949fcb84
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2021-01-06 17:43:49 -0800

    small fix

--- pyompa/__init__.py
+++ pyompa/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import division, print_function
 from .ompacore import OMPAProblem 
 from .thermocline_array import ThermoclineArrayOMPAProblem 
-from .watermasspenaltyfunc get_wm_penalty_func
+from .watermasspenaltyfunc import get_wm_penalty_func
