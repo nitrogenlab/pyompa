228e23f7d7d44aba4ddec7df1514fb70209f6db9
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2021-01-06 17:54:49 -0800

    small fix

--- pyompa/__init__.py
+++ pyompa/__init__.py
@@ -2,3 +2,6 @@ from __future__ import division, print_function
 from .ompacore import OMPAProblem 
 from .thermocline_array import ThermoclineArrayOMPAProblem 
 from .watermasspenaltyfunc import get_wm_penalty_func
+from .plotting import (plot_water_mass_fractions, plot_residuals,
+                       plot_thermocline_water_mass_fractions,
+                       plot_thermocline_residuals)
