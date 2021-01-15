e431598ec61a70904dc02861536b60b49cbd6f9c
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2021-01-15 11:31:18 -0800

    fix setup for sdist

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
+from setuptools import find_packages
 from distutils.core import setup
 
 if __name__== '__main__':
     setup(include_package_data=True,
           description='Python OMPA',
           long_description="""Python package for conducting OMPA (Optimum Multiparameter Water Mass Analysis)""",
+          author="Avanti Shrikumar",
+          author_email="avanti.shrikumar@gmail.com",
           url='https://github.com/nitrogenlab/pyompa',
           version='0.3.0.0',
-          packages=['pyompa'],
+          packages=find_packages(),
           setup_requires=[],
           install_requires=['numpy', 'pandas', 'cvxpy', 'scipy', 'toml'],
           extras_require={'altair': ['altair']},
