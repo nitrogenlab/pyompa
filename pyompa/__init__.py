e681bd1018b7f900a33ec75525e677752b5c320f
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2021-01-29 12:27:39 -0800

    added run ompa from toml to init

--- pyompa/__init__.py
+++ pyompa/__init__.py
@@ -8,3 +8,4 @@ from .plotting import (plot_ompasoln_endmember_fractions,
                        plot_thermocline_endmember_fractions,
                        plot_thermocline_residuals,
                        build_altair_viz, build_thermocline_altair_viz)
+from .parse_config import run_ompa_given_toml_config_file
