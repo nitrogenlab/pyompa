b86d78228465705e364a25a3299dc5f95de37cd2
Av Shrikumar <avanti.shrikumar@gmail.com>
2021-10-22 18:38:52 -0700

    debug

--- pyompa/endmemberpenaltyfunc.py
+++ pyompa/endmemberpenaltyfunc.py
@@ -18,6 +18,7 @@ def get_exponential_from_bounds_func(alpha, beta,
         print(lowerbound)
         print(upperbound)
         print(x, type(x))
+        print(alpha*np.maximum(0, np.maximum(lowerbound-x, x-upperbound))) 
         return np.minimum(magnitudelimit, beta*(np.exp(
         alpha*np.maximum(0, np.maximum(lowerbound-x, x-upperbound)))-1))
     return func
