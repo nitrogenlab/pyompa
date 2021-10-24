868a53277eb320d64f9b0767d4fdee92a037a4fe
Av Shrikumar <avanti.shrikumar@gmail.com>
2021-10-24 15:05:45 -0700

    using iloc

--- pyompa/endmemberpenaltyfunc.py
+++ pyompa/endmemberpenaltyfunc.py
@@ -23,7 +23,7 @@ def get_exponential_from_bounds_func(alpha, beta,
         print((alpha*np.maximum(0, np.maximum(lowerbound-x, x-upperbound))).dtype)
         print(np.exp(alpha*np.maximum(0, np.maximum(lowerbound-x, x-upperbound))))
         return np.minimum(magnitudelimit, beta*(np.exp(
-        alpha*np.maximum(0, np.maximum(lowerbound-x, x-upperbound)))-1))
+         alpha*np.maximum(0, np.maximum(lowerbound-x, x-upperbound)))-1))
     return func
     #return lambda x: np.minimum(magnitudelimit, beta*(np.exp(
     #    alpha*np.maximum(0, np.maximum(lowerbound-x, x-upperbound)))-1))
